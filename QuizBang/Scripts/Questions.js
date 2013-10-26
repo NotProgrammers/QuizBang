46f32101f792e3a87dcaeb259627a69a49647b85
alexlea <alex.lea@gmail.com>
2013-10-26 17:39:14 +0100

    Add some more questions

--- QuizBang/Scripts/Questions.js
+++ QuizBang/Scripts/Questions.js
@@ -6,7 +6,31 @@
         answer3: 'Large',
         answer4: 'Huge',
         correct_answer: 4
-    }
+    },
+    {
+        question: 'How many comments does the best code have?',
+        answer1: 'None, obvs.',
+        answer2: 'Some',
+        answer3: 'Every other line',
+        answer4: 'Every line',
+        correct_answer: 1
+    },
+    {
+        question: 'Proper programmers use?',
+        answer1: 'PHP',
+        answer2: 'Java',
+        answer3: 'Ruby',
+        answer4: 'C hash',
+        correct_answer: 4
+    }, 
+    {
+        question: 'Proper programmers use?',
+        answer1: 'PHP',
+        answer2: 'Java',
+        answer3: 'Ruby',
+        answer4: 'C hash',
+        correct_answer: 4
+    },
 ]
 
 //answers: [
