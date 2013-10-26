974248c2e1b19cde3faf9c26f8e4ae5b9b60fb10
Andrew Seward <andrew@andrewseward.co.uk>
2013-10-26 17:37:45 +0100

    more quiz questions

--- QuizBang/Scripts/Questions.js
+++ QuizBang/Scripts/Questions.js
@@ -6,7 +6,47 @@
         answer3: 'Large',
         answer4: 'Huge',
         correct_answer: 4
-    }
+    },
+    {
+        question: 'Which of the following comic book characters can be found on a HackManchester pinball machine?',
+        answer1: 'Superman',
+        answer2: 'Wolverine',
+        answer3: 'The Incredible Hulk',
+        answer4: 'Robin',
+        correct_answer: 2
+    },
+    {
+        question: 'In which gallery at the Museum of Science and Industry will you find the Beyer-Garratt articulated steam locomotive?',
+        answer1: 'The Age of Steam Gallery',
+        answer2: 'The Rail Yard',
+        answer3: 'The Revolution Manchester Gallery',
+        answer4: 'The Power Hall',
+        correct_answer: 4
+    },
+    {
+        question: 'How many packs of Refreshing Wipes did you get in your HackManchester comfort kit?',
+        answer1: '1',
+        answer2: '2',
+        answer3: '3',
+        answer4: '4',
+        correct_answer: 2
+    },
+    {
+        question: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: '',
+        correct_answer: 2
+    },
+    {
+        question: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: '',
+        correct_answer: 2
+    },
 ]
 
 //answers: [
