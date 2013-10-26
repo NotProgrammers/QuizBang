149868db5ead21248cb8c3d0499cd3bb5e2003bc
alexlea <alex.lea@gmail.com>
2013-10-26 19:04:30 +0100

    add test page

---
+++ QuizBang.ManualTest/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QuizBang.ManualTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void SendOneMessage()
+        {
+            Uri uri = new Uri("http://localhost/");
+            
+
+            WebRequest request = HttpWebRequest.CreateHttp(uri)
+        }
+    }
+}
