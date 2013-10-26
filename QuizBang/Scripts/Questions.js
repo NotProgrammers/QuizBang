968b80609ab433062ef385b8b82a6424da768de4
Alex Lea <alex.lea@esendex.com>
2013-10-26 17:07:52 +0100

    Add initial questions file for Andrew

---
+++ QuizBang/Scripts/Questions.js
@@ -0,0 +1,19 @@
+﻿var questions = [
+    {
+        question: 'How big is your momma?',
+        answers: [
+            {
+                answer: 'Tiny'
+            },
+            {
+                answer: 'Medium'
+            },
+            {
+                answer: 'Large'
+            },
+            {
+                answer: 'Enormous'
+            }
+        ]
+    }
+]
