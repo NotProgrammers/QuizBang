c4c7d1e1ceb220ce50d99da559a048ce84802079
alexlea <alex.lea@gmail.com>
2013-10-27 01:15:05 +0100

    Add w for winscreen shortcut

--- QuizBang/Scripts/quizbang.js
+++ QuizBang/Scripts/quizbang.js
@@ -21,3 +21,25 @@ function nextQuestion() {
     $('#answer-d').html(question.answerD);
     $('#answer').val(question.correct_answer);
 }
+
+function winScreen() {
+    bubblesMain(new Object({
+        type: 'linear',
+        revolve: 'center',
+        minSpeed: 100,
+        maxSpeed: 500,
+        minSize: 50,
+        maxSize: 150,
+        num: 100,
+        colors: new Array('#FFFFFF', '#BBBBBB')
+    }));
+    cycleBackgroundColors();
+}
+
+function cycleBackgroundColors() {
+    var colors = ["green", "yellow", "red", "purple", "blue"];
+    var i = 0;
+    while (i < colors.length) {
+        $("body").animate({ backgroundColor: colors[i++ % colors.length] }, 200, cycleBackgroundColors);
+    }
+}
